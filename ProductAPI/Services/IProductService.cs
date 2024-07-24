dc53a0c2d99041b2262d43242f98b7eaa971b2b5
Vanessa Klein <VANESSAL.K@hotmail.com>
2024-07-24 10:11:03 -0300

    add product service

---
+++ ProductAPI/Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using ProductAPI.Domain.Entities;
+
+namespace ProductAPI.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetAllProducts();
+        Task<Product> GetProductById(int id);
+        Task AddProduct(Product product);
+        Task UpdateProduct(Product product);
+        Task DeleteProduct(int id);
+    }
+}
